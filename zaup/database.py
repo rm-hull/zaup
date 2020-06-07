8e8030e197d83fbb5d96079e49a7f766ae739143
Richard Hull <rm_hull@yahoo.co.uk>
2020-06-07 09:30:46 +0100

    Support alt. datasource: Google Authenticator migration URI (#1)
    Protobuf definition attribution: https://alexbakker.me/post/parsing-google-auth-export-qr-code.html

--- zaup/database.py
+++ zaup/database.py
@@ -4,8 +4,10 @@
 # See LICENSE.md for details.
 
 import sqlite3
+from base64 import b32decode
 
 import const as account
+from migration_payload_pb2 import MigrationPayload
 
 # 'Account' table fields/offsets
 account._id = 0
@@ -18,8 +20,19 @@ account.issuer = 6
 account.original_name = 7
 
 
+def _convert(row):
+    secret = MigrationPayload.OtpParameters()
+    secret.name = row[account.email]
+    secret.issuer = row[account.issuer]
+    secret.secret = b32decode(row[account.secret].upper())
+    secret.type = MigrationPayload.OtpType.OTP_TYPE_TOTP
+    secret.counter = row[account.counter]
+    secret.digits = MigrationPayload.DigitCount.DIGIT_COUNT_SIX
+    return secret
+    
+
 def load_secrets(filename):
     print("Loading sqlite3 database from:", filename)
     with sqlite3.connect(filename) as conn:
         cur = conn.cursor()
-        return list(cur.execute("SELECT * FROM accounts"))
+        return [_convert(row) for row in cur.execute("SELECT * FROM accounts")]
---
+++ zaup/google_auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017-18 Richard Hull and contributors
+# See LICENSE.md for details.
+
+import base64
+
+from migration_payload_pb2 import MigrationPayload
+
+
+def load_secrets(uri):
+    if not (uri and uri.startswith("otpauth-migration://offline?data=")):
+        return None
+        
+    print("loading secrets from GoogleAuthenticator URI")
+    data = base64.b64decode(uri[33:])
+    payload = MigrationPayload()
+    payload.ParseFromString(data)
+    return list(payload.otp_parameters)
