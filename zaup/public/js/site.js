efc85636aad5b356c668a6ad1281df0aed681214
Richard Hull <rm_hull@yahoo.co.uk>
2020-10-02 20:26:08 +0100

    Add a glow when the 6-digit code changes

--- zaup/public/js/site.js
+++ zaup/public/js/site.js
@@ -10,35 +10,44 @@ var handle = null;
 var timerId = null;
 
 function getColor(timeLeft) {
-    if (timeLeft > 10 || timeLeft === 1) return "#6fdb6f";
-    else if (timeLeft > 5) return "orange";
-    else return "red";
+  if (timeLeft > 10 || timeLeft === 1) return "#6fdb6f";
+  else if (timeLeft > 5) return "orange";
+  else return "red";
 }
 
 function formatTime(time) {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
 
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
-    }
-  
-    return `${minutes}:${seconds}`;
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
   }
-  
+
+  return `${minutes}:${seconds}`;
+}
+
+function glow(elem, duration) {
+  elem.classList.add("glow");
+  setTimeout(() => elem.classList.remove("glow"), duration || 500);
+}
+
 function updateTokens() {
   fetch('/tokens')
     .then(response => response.json())
     .then(data => {
-        timeLeft.innerHTML = formatTime(data.timeLeft);
-        circle.style.strokeDashoffset = initialOffset - ((data.timeLeft-1) * slice);
-        circle.style.stroke = getColor(data.timeLeft);
-        circle.style.fill = "rgba(50,50,50,0.15)";
-        
-        data.tokens.forEach((token, index) => {
-            const elem = document.getElementById("anchor-" + index);
-            elem.innerHTML = token;
-        })
+      timeLeft.innerHTML = formatTime(data.timeLeft);
+      circle.style.strokeDashoffset = initialOffset - ((data.timeLeft - 1) * slice);
+      circle.style.stroke = getColor(data.timeLeft);
+      circle.style.fill = "rgba(50,50,50,0.15)";
+
+      data.tokens.forEach((token, index) => {
+        const elem = document.getElementById("anchor-" + index);
+        if (data.timeLeft === 30) {
+          glow(elem);
+        }
+        elem.innerHTML = token;
+      })
+
     })
     .catch(err => {
       if (handle) {
@@ -50,7 +59,7 @@ function updateTokens() {
     });
 }
 
-function showSnackbarMessage(message, duration=5000) {
+function showSnackbarMessage(message, duration = 5000) {
   const snackbar = document.getElementById('snackbar');
   document.getElementById("snackbar-message").innerText = message;
   snackbar.classList.add("show");
@@ -65,7 +74,7 @@ function init() {
   clipboard.on('success', e => showSnackbarMessage(`Copied "${e.text}" to the clipboard.`));
   updateTokens();
   handle = setInterval(updateTokens, interval);
-  
+
   // Stop annoying flicker of green snackbar message on startup
   const snackbar = document.getElementById('snackbar');
   snackbar.classList.remove("hide");
