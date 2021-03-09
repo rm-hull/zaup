3e70f59b739cef23f4b0aa238aca5fa49d1fc19d
Richard Hull <rm_hull@yahoo.co.uk>
2021-03-09 17:36:17 +0000

    Allow config.google_auth to be string or array

--- zaup/google_auth.py
+++ zaup/google_auth.py
@@ -4,21 +4,28 @@
 # See LICENSE.md for details.
 
 import base64
+import itertools
 
 from migration_payload_pb2 import MigrationPayload
+
 
 def _normalize(secret):
     if ':' in secret.name:
         pos = secret.name.index(":")
         secret.issuer = secret.name[:pos]
         secret.name = secret.name[pos + 1:]
-        
+
     return secret
 
+
 def load_secrets(uri):
+
+    if isinstance(uri, list):
+        return list(itertools.chain.from_iterable(load_secrets(each) for each in uri))
+
     if not (uri and uri.startswith("otpauth-migration://offline?data=")):
         return None
-        
+
     print("loading secrets from GoogleAuthenticator URI")
     data = base64.b64decode(uri[33:])
     payload = MigrationPayload()
